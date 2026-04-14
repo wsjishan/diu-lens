18cb5f331394224df27feaefeea56ebdf5284f97
G.M Kibria Rafi <123581729+kibria-rafi@users.noreply.github.com>
2026-04-14 12:45:16 +0600

    fix: add suppressHydrationWarning to body element in RootLayout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       lang="en"
       className={`${dmSans.variable} ${spaceGrotesk.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>{children}</body>
     </html>
   );
 }
